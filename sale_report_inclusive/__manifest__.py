b0feb6c6f956696f67518d728fe01c302619da40
Terence Nzaywa <progterrence@gmail.com>
2019-12-05 18:40:40 +0300

    [FIX] Add some missing files

---
+++ sale_report_inclusive/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Inclusive Sales Reports",
+    'version': '12.0.1.0',
+    'category': 'Point of Sale',
+    'author': 'Sunflower IT',
+    'website': 'http://sunflowerweb.nl',
+    'sequence': 0,
+    'depends': [
+        'point_of_sale',
+        'hotel_reservation'
+    ],
+    'data': [
+        'reports/pos_report.xml',
+        'reports/report_saledetails.xml',
+        'views/pos_reports.xml',
+        'wizards/pos_details.xml'
+        # 'views/res_users.xml',
+        # 'wizards/remove_pos_order.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
